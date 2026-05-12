32dd1e9da65f6493f328d96a76703f45efe6d340
tippajohnw-dev <tippajohnw@gmail.com>
2026-05-12 21:08:00 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'capybarabu-v1.0.9';
+const CACHE = 'capybarabu-v1.1.0';
 const SHELL = ['./index.html', './manifest.json', './capybarabu%20inc.png', './Icon%20Emotional.png'];
 
 const SKIP_HOSTS = [
